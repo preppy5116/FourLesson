78e17116c26708cf025e663a11ea46701b94f4c7
Shipulina <61729850+preppy5116@users.noreply.github.com>
2024-01-09 13:49:50 +0300

    Delete FourLesson/src directory

--- FourLesson/src/AccountIsLockedException.java
+++
@@ -1,9 +0,0 @@
-public class AccountIsLockedException extends Throwable{
-    public AccountIsLockedException() {
-        System.out.println("Слишком много попыток, Аккаунт заблокирован на 10 секунд");
-    }
-
-    public AccountIsLockedException(String message) {
-        super(message);
-    }
-}
--- FourLesson/src/MinCashException.java
+++
@@ -1,5 +0,0 @@
-public class MinCashException extends Throwable{
-    public MinCashException() {
-        System.out.println("Сумма должна быть кратна 100, введите другую сумму");
-    }
-}
